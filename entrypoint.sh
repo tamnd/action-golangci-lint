7f4be21f0e9e7239b98689e981b3eb41689cffd6
tamnd <tamnd87@gmail.com>
2019-10-31 21:13:53 +0700

    Printing version

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+echo "Reviewdog version: $(reviewdog -version)"
+echo "GoglangCI-lint version: $(golangci-lint version)"
 cd ${GITHUB_WORKSPACE}/${INPUT_WORKSPACE}
 GO111MODULE=on golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
   | reviewdog -f=golangci-lint -name="${INPUT_TOOL_NAME}" -reporter=github-pr-check -level="${INPUT_LEVEL}"
