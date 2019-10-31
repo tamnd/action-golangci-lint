186bb578fdaa23a959bb6d063014db719ff5e433
tamnd <tamnd87@gmail.com>
2019-10-31 13:06:34 +0700

    Add Go Workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+cd ${GITHUB_WORKSPACE}/${INPUT_WORKSPACE}
 GO111MODULE=on golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
   | reviewdog -f=golangci-lint -name="${INPUT_TOOL_NAME}" -reporter=github-pr-check -level="${INPUT_LEVEL}"
