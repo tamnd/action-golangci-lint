477e84f1ea13df13aa626e12fd6fabe4ca44400a
tamnd <tamnd87@gmail.com>
2019-12-08 10:05:25 +0700

    Customize action-golangci-lint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,12 @@ cd "$GITHUB_WORKSPACE" || exit 1
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # shellcheck disable=SC2086
-golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
-  | reviewdog -f=golangci-lint -name="${INPUT_TOOL_NAME}" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+echo "Reviewdog version: $(reviewdog -version)"
+echo "GoglangCI-lint version: $(golangci-lint version)"
+
+cd "${GITHUB_WORKSPACE}"/"${INPUT_WORKSPACE}" || exit 1
+echo "Running on ${GITHUB_WORKSPACE}/${INPUT_WORKSPACE}"
+ls
+
+GO111MODULE=on golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
+  | reviewdog -f=golangci-lint -name="${INPUT_TOOL_NAME}" -reporter=${INPUT_REPORTER:-github-pr-check} -level="${INPUT_LEVEL}"
